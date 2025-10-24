3c953d7ec2724c9a5a00caec8181b8c5cb884845
Gauthier-det <gauthier.detroussel@gmail.com>
2025-10-24 13:34:52 +0200

    add gitignore and .env

--- backend/src/Question.js
+++ backend/src/Question.js
@@ -1,10 +1,16 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "culturegoat",       
-    password: "GaLuBaRaGOAT", 
-    database: "culturegoat",  
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
 });
 
 class Question {
